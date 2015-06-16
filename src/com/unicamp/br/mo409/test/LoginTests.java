676beb8c1f11629c3fa6e72fecb8e558ba6c4d32
liborio <pedroliborio@live.com>
2015-06-16 00:22:15 -0300

    Mudancas Robotium

--- src/com/unicamp/br/mo409/test/LoginTests.java
+++ src/com/unicamp/br/mo409/test/LoginTests.java
@@ -3,10 +3,13 @@ package com.unicamp.br.mo409.test;
 import org.junit.Before;
 import org.junit.Test;
 
+import android.R;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
 
 import com.robotium.solo.Solo;
 import com.unicamp.br.mo409.controller.LoginActivity;
+import com.unicamp.br.mo409.controller.MainActivity;
 
 public class LoginTests extends ActivityInstrumentationTestCase2<LoginActivity> {
 
@@ -26,9 +29,15 @@ public class LoginTests extends ActivityInstrumentationTestCase2<LoginActivity> 
 	}
 
 	@Test
-	public void testLogin() {
+	public void testLogin() throws InterruptedException {
+		//primeira tela de Login quando o sistema inicia
 		solo.assertCurrentActivity("Tela de Login", LoginActivity.class);
-		//solo.
+		solo.clickOnEditText(0);
+		solo.enterText(0, "Eliane");
+		solo.clickOnEditText(1);
+		solo.enterText(1, "12345");
+		solo.clickOnButton(0);
+		solo.waitForActivity("Disciplinas");
 	}
 
 }
